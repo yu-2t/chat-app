834813d5968af361750d48159a79f0b29ef24ae0
yu-2t <yk.19861210@gmail.com>
2020-10-10 12:36:42 +0900

    usersテーブルの作成

--- db/migrate/20201010032746_devise_create_users.rb
+++ db/migrate/20201010032746_devise_create_users.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-class DeviseCreateInstalls < ActiveRecord::Migration[6.0]
+class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :installs do |t|
+    create_table :users do |t|
       ## Database authenticatable
+      t.string :name,                null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -36,9 +37,9 @@ class DeviseCreateInstalls < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :installs, :email,                unique: true
-    add_index :installs, :reset_password_token, unique: true
-    # add_index :installs, :confirmation_token,   unique: true
-    # add_index :installs, :unlock_token,         unique: true
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_index :users, :unlock_token,         unique: true
   end
 end
